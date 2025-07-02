2ba938284fd376f05b7d448e3d1821b264c29822
Saurabh Singh <saurabh26sep2000@gmail.com>
2025-07-03 00:22:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'
+
+// const API = 'http://localhost:5000/api/users';
+const API = 'https://user-manager-backend.onrender.com/api/users';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [form, setForm] = useState({ name: '', email: '' });
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const res = await axios.get(API);
+    setUsers(res.data);
+  };
+
+  const addUser = async (e) => {
+    e.preventDefault();
+    const res = await axios.post(API, form);
+    setUsers([...users, res.data]);
+    setForm({ name: '', email: '' });
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`${API}/${id}`);
+    setUsers(users.filter(user => user.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }} className='App'>
+      <h1>User Manager</h1>
+      <form onSubmit={addUser}>
+        <input
+          placeholder="Name"
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+          required
+        />
+        <br/>
+        <input
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          required
+        />
+        <br/>
+        <button type="submit">Add User</button>
+      </form>
+
+      <ul>
+        {users.map(user => (
+          <li key={user.id} style={{listStyle: 'none'}}>
+            {user.name} ({user.email})
+            <button onClick={() => deleteUser(user.id)} style={{ marginLeft: 10 }}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

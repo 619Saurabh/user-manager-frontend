0081190ca42edb9ba244d6b2c6b9e7e9fba23bff
Saurabh Singh <saurabh26sep2000@gmail.com>
2025-07-03 10:59:52 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const API = 'http://localhost:5000/api/users';
 function App() {
   const [users, setUsers] = useState([]);
   const [form, setForm] = useState({ name: '', email: '' });
+  const [editUserId, setEditUserId] = useState(null); // null -> add mode
 
   useEffect(() => {
     fetchUsers();
@@ -17,22 +18,50 @@ function App() {
     setUsers(res.data);
   };
 
-  const addUser = async (e) => {
+  // const addUser = async (e) => {
+  //   e.preventDefault();
+  //   const res = await axios.post(API, form);
+  //   setUsers([...users, res.data]);
+  //   setForm({ name: '', email: '' });
+  // };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await axios.post(API, form);
-    setUsers([...users, res.data]);
+
+    if (editUserId) {
+      // Edit user (PUT)
+      const res = await axios.put(`${API}/${editUserId}`, form);
+      setUsers(users.map((user)=> user.id === editUserId ? res.data : user));
+      setEditUserId(null); // switch back to add mode
+    } else {
+      // Add user (POST)
+      const res = await axios.post(API, form);
+      setUsers([...users, res.data]);
+    }
+
     setForm({ name: '', email: '' });
   };
 
   const deleteUser = async (id) => {
     await axios.delete(`${API}/${id}`);
-    setUsers(users.filter(user => user.id !== id));
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const startEdit = (user) => {
+    setForm({ name: user.name, email: user.email });
+    setEditUserId(user.id);
+  };
+
+  const cancelEdit = () => {
+    setEditUserId(null);
+    setForm({ name: '', email: '' });
   };
 
   return (
     <div style={{ padding: 20 }} className='App'>
       <h1>User Manager</h1>
-      <form onSubmit={addUser}>
+
+      <form onSubmit={handleSubmit}>
         <input
           placeholder="Name"
           value={form.name}
@@ -47,13 +76,17 @@ function App() {
           required
         />
         <br/>
-        <button type="submit">Add User</button>
+        <button type="submit">{editUserId ? 'Update' : 'Add'}</button>
+        {editUserId && (
+          <button type="button" onClick={cancelEdit} style={{ marginLeft: 10 }}>Cancel</button>
+        )}
       </form>
 
       <ul>
         {users.map(user => (
           <li key={user.id} style={{listStyle: 'none'}}>
             {user.name} ({user.email})
+             <button onClick={() => startEdit(user)} style={{ marginLeft: 10 }}>Edit</button>
             <button onClick={() => deleteUser(user.id)} style={{ marginLeft: 10 }}>Delete</button>
           </li>
         ))}
@@ -63,3 +96,4 @@ function App() {
 }
 
 export default App;
+
